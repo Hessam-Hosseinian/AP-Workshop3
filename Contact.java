5ded8192b07b4468327803c81575741649efa722
Hessam <hessam2004h@gmail.com>
2024-03-11 19:07:50 +0330

    Contact Class

---
+++ Contact.java
@@ -0,0 +1,78 @@
+public class Contact {
+    private String group;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private PhoneNumber phoneNumber;
+    private Address address;
+
+    public Contact(String group, String email, String firstName, String lastName,
+            PhoneNumber phoneNumber, Address address) {
+        this.group = group;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getGroup() {
+        return this.group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public PhoneNumber getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(PhoneNumber phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Address getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String toString() {
+        return "Contact{" + "\n" +
+                "group= " + group + '\n' +
+                "email= " + email + '\n' +
+                "firstName= " + firstName + '\n' +
+                "lastName= " + lastName + '\n' +
+                "phoneNumber= " + phoneNumber + '\n' +
+                "address= " + address + '\n' +
+                '}';
+    }
+
+}
