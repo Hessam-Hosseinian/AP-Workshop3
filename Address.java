1110638f3e2fef6a1d05607a337100f2f04f2a37
Hessam <hessam2004h@gmail.com>
2024-03-11 14:06:46 +0330

    Address

---
+++ Address.java
@@ -0,0 +1,45 @@
+public class Address {
+
+    private String zipCode;
+    private String country;
+    private String city;
+
+    public Address(String zipCode, String country, String city) {
+        this.zipCode = zipCode;
+        this.country = country;
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return this.zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String toString() {
+        return "{" +
+                " zipCode='" + getZipCode() + "'" +
+                ", country='" + getCountry() + "'" +
+                ", city='" + getCity() + "'" +
+                "}";
+    }
+
+}
